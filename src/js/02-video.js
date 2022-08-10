fb411bfd4aabb7affa4f83117c95a42055d35359
SergeyKartavcev <102188021+SergeyKartavcev@users.noreply.github.com>
2022-08-10 14:11:20 +0300

    add
    вніс правки в другу і третю роботи

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,20 +2,18 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
 auditLocalStorage();
 
 player.on('timeupdate', throttle(currentTime, 1000));
 
 
 function currentTime({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+  localStorage.setItem(localStorageKey, seconds);
   console.log(seconds);
-}
+};
 
 function auditLocalStorage() {
-  if ('videoplayer-current-time' === undefined || 'videoplayer-current-time' === null) {
-   
-  }
-  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-  console.log(localStorage.getItem('videoplayer-current-time'));
-}
+  player.setCurrentTime(localStorage.getItem(localStorageKey));
+  console.log(localStorage.getItem(localStorageKey));
+};
